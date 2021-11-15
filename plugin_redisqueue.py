468c39eab42e62ced253fbd9e7ab8da21d0fa9e3
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2021-11-15 23:29:01 +0100

    Push odometer info to redis as well

--- plugin_redisqueue.py
+++ plugin_redisqueue.py
@@ -18,7 +18,8 @@ class Plugin(MyTPlugin):
             redis_url_str = os.getenv("REDIS_URL")
             log.debug("Loaded REDIS_URL = %s", redis_url_str)
         redis_url = urlparse(redis_url_str)
-        self._redis_key = Myt.get_environment_or_filedata(config_data, "redis_key") or "trip_queue"
+        self._trip_key = Myt.get_environment_or_filedata(config_data, "redis_trip_key") or "trip_queue"
+        self._odometer_key = Myt.get_environment_or_filedata(config_data, "redis_odometer_key") or "odometer_queue"
         
         # init redis
         self._redis = redis.Redis(host=redis_url.hostname, password=redis_url.password, port=redis_url.port, db=0, decode_responses=True)
@@ -26,11 +27,14 @@ class Plugin(MyTPlugin):
     def trip_data(self, trip_id, trip_data):
         print("Pushing trip with ID {} to redis".format(trip_id))
         json_str = json.dumps(trip_data)
-        self._redis.rpush(self._redis_key, json_str)
-    
-    def trip_data_done(self):
-        pass
+        self._redis.rpush(self._trip_key, json_str)
 
     def odometer(self, fresh, odometer, odometer_unit, fuel_percent) -> None:
-        pass
+        odometer_data = {
+            "odometer": odometer,
+            "odometer_unit": odometer_unit,
+            "fuel_percent": fuel_percent
+        }
+        json_str = json.dumps(odometer_data)
+        self._redis.rpush(self._odometer_key, json_str)
     
